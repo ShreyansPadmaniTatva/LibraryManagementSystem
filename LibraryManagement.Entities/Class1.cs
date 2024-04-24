858f3dcee72cb03990a3b348e01916acabdb277f
shreyans-padmani <shreyans.padmani@etatvasoft.com>
2024-04-24 13:55:02 +0530

    Add project files.

---
+++ LibraryManagement.Entities/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryManagement.Entities
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ LibraryManagement.Repositories/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryManagement.Repositories
+{
+    public class Class1
+    {
+
+    }
+}
